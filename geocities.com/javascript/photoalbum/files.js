459c9069190b0ebc133033eab1dba3251eb7ab0e
FLEISCHMANN György <gyorgy@fleischmann.hu>
2017-10-10 12:41:57 +0200

    Test2

---
+++ geocities.com/javascript/photoalbum/files.js
@@ -0,0 +1,68 @@
+
+PicturesDB[PicCounter++] = new PicStruct(
+  "Testtitle1", 
+  "http://www.linuxportal.hu/img/header/header_11.jpg", 
+  "Anna", 
+  "Testtext1",
+  "W",
+  "H",  
+  "Dfgffgfg",
+  "8fgfg",
+  "9",
+  "0",
+  "11",
+  "12",
+  "13",
+  1
+);
+	
+PicturesDB[PicCounter++] = new PicStruct(
+  "Test title2",
+  "02.jpg",
+  "Flex", 
+  "Test text2",
+  "",
+  "",
+  "",  
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  1
+);
+
+PicturesDB[PicCounter++] = new PicStruct(
+  "Test title3",
+  "http://flexoft.dyndns.org/mirrors/anna.fleischmann.hu/anna_pics/Anna_0004.jpg",
+  "Flex", 
+  "Test text3",
+  "",
+  "",
+  "",  
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  1
+);
+
+PicturesDB[PicCounter++] = new PicStruct(
+  "Test title4",
+  "02.jpg",
+  "x", 
+  "",
+  "",
+  "",
+  "",
+  "",  
+  "",
+  "",
+  "",
+  "",
+  "",
+  1
+);
